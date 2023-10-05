6919217590fd15e0c635d5911ad23ce029944615
mdarora <mdarora0001@gmail.com>
2023-10-05 00:46:40 -0400

    moved db connects at the top of every route

--- app/api/todos/route.ts
+++ app/api/todos/route.ts
@@ -3,21 +3,24 @@ import connectDB from "@/db/dbConfig";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest){
-    const {title, description} = await req.json();
     await connectDB();
+
+    const {title, description} = await req.json();
     await Todo.create({title, description});
     return NextResponse.json({message: "Todo created."}, {status: 201});
 }
 
 export async function GET(){
-    const todos = await Todo.find({});
     await connectDB();
+
+    const todos = await Todo.find({});
     return NextResponse.json([...todos],{ status : 200 });
 }
 
 export async function DELETE(req: NextRequest){
-    const id = req.nextUrl.searchParams.get('id');
     await connectDB();
+    
+    const id = req.nextUrl.searchParams.get('id');
     await Todo.findByIdAndDelete(id);
     return NextResponse.json({message: "Todo Deleted"}, {status: 200});
 }
