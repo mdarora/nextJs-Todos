8209569c85c609031abcf68f53ab3fe4bc7cf4df
mdarora <mdarora0001@gmail.com>
2023-10-05 00:47:14 -0400

    getting all todos from apiu

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,33 @@
 import Image from 'next/image'
-import Todo from './components/Todo'
+import Todo from './components/Todo';
 
-export default function Home() {
+interface todo {
+  _id: string,
+  title: string,
+  description: string
+}
+
+const getTodos = async ()=>{
+  try {
+    const res = await fetch(`http://127.0.0.1:3000/api/todos`, {
+      cache: 'no-store'
+    });
+    if(!res.ok) throw new Error('Something went wrong');
+    return res.json();
+  } catch (error) {
+    console.log('error loading todos : ', error);
+  }
+}
+
+export default async function Home() {
+  const todos = await getTodos();
   return (
     <>
       <section className='max-w-3xl mx-auto pt-4'>
         <h1 className='text-4xl my-4 font-bold'>Todo List</h1>
-        <Todo/>
+        {todos.map((todo: todo) => {
+          return <Todo key={todo._id} id={todo._id} title={todo.title} description={todo.description} />;
+        })}
       </section>
     </>
   )
